802a8a51f15a982d1d5a3aed5ee7cdbbf69f12ce
sbellotlecoq <sbellotlecoq@gmail.com>
2023-02-13 21:11:28 +0100

    first name ok

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -2,8 +2,8 @@ class StaticPageController < ApplicationController
   def home
   @user = User.all.sample
   @gossips = Gossip.all
-  
   @comment = Comment.all
+  @first_name = params[:first_name] 
   end
 
   def team
